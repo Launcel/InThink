2988a9493b089d39091bc1603a6df452de3bdd6c
Launcel <Launcel@192.168.0.111>
2017-04-10 15:16:00 +0800

    dafdsf

---
+++ src/main/java/xyz/launcel/App.java
@@ -0,0 +1,15 @@
+package xyz.launcel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	System.out.println("fsdfsdfdsff");
+        System.out.println( "Hello World!" );
+    }
+}
